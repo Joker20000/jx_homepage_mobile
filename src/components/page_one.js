b7c499ab99851696c5f66097f54614429ce4f96e
wangkaikun <wangkaikun@jia-fu.cn>
2020-04-30 16:42:29 +0800

    new change

--- src/components/page_one.js
+++ src/components/page_one.js
@@ -1,23 +1,9 @@
 import * as React from 'react';
 
 export class PageOne extends React.Component{
-    constructor (props) {
-        super (props);
-    }
-    componentDidMount () {
-        getData('https://www.baidu.com').then(res=>{
-            debugger;
-        });
-    }
     render () {
         return (
             <div className="page_one"></div>
         )
     }
-}
-
-function getData (url) {
-    return fetch(url, {
-        method: 'GET'
-    })
 }
